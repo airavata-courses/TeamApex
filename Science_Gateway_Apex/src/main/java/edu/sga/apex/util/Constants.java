0bf7e56cff8e2644a733871640755efc07689023
Gourav Shenoy <shenoy.200@gmail.com>
2016-02-06 15:45:11 -0500

    Implement SSH Utility

--- Science_Gateway_Apex/src/main/java/edu/sga/apex/util/Constants.java
+++ Science_Gateway_Apex/src/main/java/edu/sga/apex/util/Constants.java
@@ -10,6 +10,9 @@ public class Constants {
 	/** The Constant SFTP_CHANNEL. */
 	public static final String SFTP_CHANNEL = "sftp";
 	
+	/** The Constant EXEC_CHANNEL. */
+	public static final String EXEC_CHANNEL = "exec";
+	
 	/** The Constant SSH_PORT. */
 	public static final int SSH_PORT = 22;
 }
