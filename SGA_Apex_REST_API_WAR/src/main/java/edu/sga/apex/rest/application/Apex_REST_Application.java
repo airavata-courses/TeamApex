08e5b5b035d59e287cce17509e9b89f1d56edbd8
Gourav Shenoy <shenoy.200@gmail.com>
2016-02-11 23:58:18 -0500

    Create a simple API wrapper for SGA

--- SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/application/Apex_REST_Application.java
+++ SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/application/Apex_REST_Application.java
@@ -5,12 +5,14 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import edu.sga.apex.rest.resource.JobResource;
+
 public class Apex_REST_Application extends Application {
 	
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> classes = new HashSet<Class<?>>();
-		
+		classes.add(JobResource.class);
 		
 		return classes;
 	}
