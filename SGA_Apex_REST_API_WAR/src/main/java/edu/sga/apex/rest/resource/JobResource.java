8112867392f991a932e9e5ae8dbfa1d578d781bb
Gourav Shenoy <shenoy.200@gmail.com>
2016-02-20 15:11:20 -0500

    Apache Wink & Maven Jetty Integration

--- SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/resource/JobResource.java
+++ SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/resource/JobResource.java
@@ -1,10 +1,26 @@
 package edu.sga.apex.rest.resource;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
-@Path("jobs")
+@Path("/job")
 public class JobResource {
 	
-	//public Response submitJob()
+	@GET
+	@Path("/submit/{jobName}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response submitJob(@PathParam("jobName") String jobName) {
+		
+		ResponseBuilder builder = null;
+		
+		String result = "Hello World!";
+		
+		builder = Response.ok(result);
+		return builder.build();
+	}
 }
