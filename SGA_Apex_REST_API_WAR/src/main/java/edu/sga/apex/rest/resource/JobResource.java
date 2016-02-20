1728692e7f2966d79724b837d35d1f6938195838
Gourav Shenoy <shenoy.200@gmail.com>
2016-02-20 18:55:44 -0500

    Adding apache wink-json-provider dependency

--- SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/resource/JobResource.java
+++ SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/resource/JobResource.java
@@ -1,6 +1,7 @@
 package edu.sga.apex.rest.resource;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -8,17 +9,22 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
+import org.json.JSONObject;
+
 @Path("/job")
 public class JobResource {
 	
-	@GET
+	@POST
 	@Path("/submit/{jobName}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public Response submitJob(@PathParam("jobName") String jobName) {
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response submitJob(@PathParam("jobName") String jobName, JSONObject inputObj) {
 		
 		ResponseBuilder builder = null;
 		
-		String result = "Hello World!";
+		String result = "{\"status\": \"OK\"}";
+		
+		System.out.println(inputObj);
 		
 		builder = Response.ok(result);
 		return builder.build();
