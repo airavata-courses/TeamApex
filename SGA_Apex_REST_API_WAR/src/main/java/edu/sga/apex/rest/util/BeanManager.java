d55396f6452ac5d1dda41aeb36af426b264e1663
Erika Dsouza <erika27desouza@gmail.com>
2016-02-20 20:48:16 -0500

    fixed json parsing issue

--- SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/util/BeanManager.java
+++ SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/util/BeanManager.java
@@ -20,13 +20,26 @@ public class BeanManager {
 		SubmitJobRequestBean bean = new SubmitJobRequestBean();
 		if (json != null) {
 			bean.setJobName(json.getString("jobName"));
+			
 			bean.setEmailId(json.getString("emailId"));
-			bean.setNumNodes((json.getInt("numNodes") == 0) ? Constants.DEFAULT_NUM_NODES
-					: json.getInt("numNodes"));
-			bean.setNumProcessors((json.getInt("numProcessors") == 0) ? Constants.DEFAULT_NUM_PROCESSORS
-					: json.getInt("numProcessors"));
-			bean.setWallTime((json.getString("wallTime") == null) ? Constants.DEFAULT_WALLTIME
-					: json.getString("wallTime"));
+			
+			if(json.has("numNodes")){
+				bean.setNumNodes(json.getInt("numNodes"));
+			}else{
+				bean.setNumNodes(Constants.DEFAULT_NUM_NODES);
+			}
+			
+			if(json.has("numProcessors")){
+				bean.setNumProcessors(json.getInt("numProcessors"));
+			}else{
+				bean.setNumProcessors(Constants.DEFAULT_NUM_PROCESSORS);
+			}
+
+			if(json.has("wallTime")){
+				bean.setWallTime(json.getString("wallTime"));
+			}else{
+				bean.setWallTime(Constants.DEFAULT_WALLTIME);
+			}
 		}
 		return bean;
 	}
