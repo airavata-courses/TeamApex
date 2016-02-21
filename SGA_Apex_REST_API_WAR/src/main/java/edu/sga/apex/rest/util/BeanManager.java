d5a46d45f24581f8c4543ac46d2f5de9fa84098f
Erika Dsouza <erika27desouza@gmail.com>
2016-02-20 19:32:45 -0500

    added manager and pom dependenccy

---
+++ SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/util/BeanManager.java
@@ -0,0 +1,20 @@
+package edu.sga.apex.rest.util;
+
+import org.json.JSONObject;
+
+import edu.sga.apex.rest.bean.SubmitJobRequestBean;
+
+public class BeanManager {
+
+	public static SubmitJobRequestBean getSubmitJobReqBean(JSONObject json) {
+		SubmitJobRequestBean bean = new SubmitJobRequestBean();
+		if (json != null) {
+			bean.setJobName(json.getString("jobName"));
+			bean.setEmailId(json.getString("emailId"));
+			bean.setNumCores((json.getInt("numCores") == 0) ? 2 : json.getInt("numCores"));
+			bean.setNumProcessors((json.getInt("numProcessors") == 0) ? 6 : json.getInt("numProcessors"));
+			bean.setWallTime((json.getString("wallTime") == null) ? "30:00" : json.getString("wallTime"));
+		}
+		return bean;
+	}
+}
