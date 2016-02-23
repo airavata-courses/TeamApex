d71c1a698f632b666d5f05255f2ca8c7af694c01
Erika Dsouza <erika27desouza@gmail.com>
2016-02-23 16:45:39 -0500

    added doPost and doDelete client functionality

--- SGA_Apex_Client/src/main/java/edu/sga/apex/rest/client/SGAApexClient.java
+++ SGA_Apex_Client/src/main/java/edu/sga/apex/rest/client/SGAApexClient.java
@@ -2,36 +2,80 @@ package edu.sga.apex.rest.client;
 
 import java.util.Scanner;
 
+import org.apache.wink.client.ClientResponse;
+import org.json.JSONObject;
+
 public class SGAApexClient {
 	
-	public static void submitJob(){
+	public static ClientResponse submitJob(Scanner input){
+		JSONObject obj = new JSONObject();
+		JSONObject request = new JSONObject();
 		
+		System.out.println("Please enter the number of processors.");
+		String numProcessors = input.next();
+		if(numProcessors.equals("") || numProcessors.isEmpty()){
+			numProcessors = "6";
+		}
+		obj.put("numProcessors", numProcessors);
+		
+		System.out.println("Please enter the email id");
+		String emailId = input.next();
+		obj.put("emailId", emailId);
+		
+		System.out.println("Please enter the number of nodes.");
+		String numNodes = input.next();
+		if(numNodes.equals("") || numNodes.isEmpty()){
+			numNodes = "2";
+		}
+		obj.put("numNodes", numNodes);
+		
+		System.out.println("Please enter the wall time");
+		String wallTime = input.next();
+		if(wallTime.equals("") || wallTime.isEmpty()){
+			wallTime = "30:00";
+		}
+		obj.put("wallTime", wallTime);
+		
+		System.out.println("Please enter the job name");
+		String jobName = input.next();
+		obj.put("jobName", jobName);
+		
+		request.put("submitJobRequest", obj);
+		
+		return APIUtil.doPost(request);
 	}
 	
 	public static void monitorJob(){
 		
 	}
 	
-	public static void deleteJob(){
-		
+	public static ClientResponse deleteJob(Scanner input){
+		System.out.println("Please enter the job id");
+		String jobId = input.next();
+		return APIUtil.doDelete(jobId);
 	}
 
 	public static void main(String[] args) {
 		int choice;
+		ClientResponse response;
 		Scanner input = new Scanner(System.in);
 		do
 		{	
-			System.out.println("Please enter 1 - Submit job, 2 - Monitor job, 3 - Cancel job and 4 - exit");
+			System.out.println("Please enter your choice: \n 1 - Submit job \n 2 - Monitor job \n 3 - Cancel job \n 4 - exit");
 			choice = input.nextInt();
 			switch(choice){
 			case 1:
-				SGAApexClient.submitJob();
+				response = SGAApexClient.submitJob(input);
+				System.out.println("Status code "+ response.getStatusCode());
+				System.out.println(response.getEntity(String.class));
 				break;
 			case 2:
 				SGAApexClient.monitorJob();
 				break;
 			case 3:
-				SGAApexClient.deleteJob();
+				response = SGAApexClient.deleteJob(input);
+				System.out.println("Status code "+response.getStatusCode());
+				System.out.println(response.getEntity(String.class));
 				break;
 			case 4:
 				break;
