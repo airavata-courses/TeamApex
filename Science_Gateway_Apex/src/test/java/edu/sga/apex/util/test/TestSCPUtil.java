159e22f60ade3bcdb33b4edcd56c0a856890d66c
Mangirish Wagle <vaglomangirish@gmail.com>
2016-03-14 23:16:21 -0400

    Adding basic form auth

--- Science_Gateway_Apex/src/test/java/edu/sga/apex/util/test/TestSCPUtil.java
+++ Science_Gateway_Apex/src/test/java/edu/sga/apex/util/test/TestSCPUtil.java
@@ -11,26 +11,25 @@ import org.junit.Test;
 
 import edu.sga.apex.bean.SCPRequestBean;
 import edu.sga.apex.util.Constants;
-import edu.sga.apex.util.SFTPUtil;
 
 public class TestSCPUtil {
-	
+
 	@Test
 	public void testSCPUtil(){
 		try {
 			Properties properties = new Properties();
 			String propFileName = "config.properties";
-			
+
 			InputStream inputStream = getClass().getClassLoader()
 					.getResourceAsStream(propFileName);
-			
+
 			if(inputStream != null) {
 				properties.load(inputStream);
 			}
 			else {
 				throw new FileNotFoundException("property file: " + propFileName + " not found!");
 			}
-			
+
 			SCPRequestBean bean = new SCPRequestBean();
 			bean.setHostName(properties.getProperty("hostname"));
 			bean.setUserName(properties.getProperty("username"));
@@ -40,17 +39,17 @@ public class TestSCPUtil {
 			bean.setSourceFilePath(properties.getProperty("srcFilePath"));
 			bean.setPrivateKeyFilePath(properties.getProperty("privateKey"));
 			bean.setKnownHostsFilePath(properties.getProperty("knownHosts"));		
-			
+
 			//SCPUtil util = new SCPUtil(bean);
 			//util.sendToServer();
-			
+
 			assertTrue(true);
 		}
 		catch(Exception ex) {
 			ex.printStackTrace();
 			fail("Test Failed!");
 		}
-				
+
 	}
 
 }
