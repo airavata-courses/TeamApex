bef95d71fcdd797783250c64abbc9ef8f51ff791
Mangirish Wagle <vaglomangirish@gmail.com>
2016-03-19 17:57:47 -0400

    Adding missing annotation

--- Science_Gateway_Apex/src/main/java/edu/sga/apex/entity/Experiment.java
+++ Science_Gateway_Apex/src/main/java/edu/sga/apex/entity/Experiment.java
@@ -26,6 +26,7 @@ public class Experiment {
 	@JoinColumn(name="machineId")
 	private Machine machine;
 
+	@Column
 	String jobName;
 
 	@ManyToOne( fetch = FetchType.LAZY, cascade = CascadeType.ALL )
