7c6707b3078f9589d99a8e642d815222767373de
Gourav Shenoy <shenoy.200@gmail.com>
2016-03-20 15:12:28 -0400

    Adding JAXB classes for User, Application & AppInput

--- SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/jaxb/User.java
+++ SGA_Apex_REST_API_WAR/src/main/java/edu/sga/apex/rest/jaxb/User.java
@@ -14,17 +14,17 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for submitJobResponse complex type.
+ * <p>Java class for user complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="submitJobResponse">
+ * &lt;complexType name="user">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="jobId" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
- *         &lt;element name="status" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="userName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="password" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -34,61 +34,61 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "submitJobResponse", propOrder = {
-    "jobId",
-    "status"
+@XmlType(name = "user", propOrder = {
+    "userName",
+    "password"
 })
-public class SubmitJobResponse {
+public class User {
 
-    protected String jobId;
-    protected String status;
+    protected String userName;
+    protected String password;
 
     /**
-     * Gets the value of the jobId property.
+     * Gets the value of the userName property.
      * 
      * @return
      *     possible object is
      *     {@link String }
      *     
      */
-    public String getJobId() {
-        return jobId;
+    public String getUserName() {
+        return userName;
     }
 
     /**
-     * Sets the value of the jobId property.
+     * Sets the value of the userName property.
      * 
      * @param value
      *     allowed object is
      *     {@link String }
      *     
      */
-    public void setJobId(String value) {
-        this.jobId = value;
+    public void setUserName(String value) {
+        this.userName = value;
     }
 
     /**
-     * Gets the value of the status property.
+     * Gets the value of the password property.
      * 
      * @return
      *     possible object is
      *     {@link String }
      *     
      */
-    public String getStatus() {
-        return status;
+    public String getPassword() {
+        return password;
     }
 
     /**
-     * Sets the value of the status property.
+     * Sets the value of the password property.
      * 
      * @param value
      *     allowed object is
      *     {@link String }
      *     
      */
-    public void setStatus(String value) {
-        this.status = value;
+    public void setPassword(String value) {
+        this.password = value;
     }
 
 }
