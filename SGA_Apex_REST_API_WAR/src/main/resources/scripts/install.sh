db1837b5fefca30067d3f7d5af489b7328642240
Gourav Shenoy <shenoy.200@gmail.com>
2016-04-19 09:45:46 -0400

    Update install.sh

--- SGA_Apex_REST_API_WAR/src/main/resources/scripts/install.sh
+++ SGA_Apex_REST_API_WAR/src/main/resources/scripts/install.sh
@@ -4,4 +4,4 @@ mvn -e clean install
 echo 'Installing SGA_Apex_REST_API_WAR'
 cd '/home/ec2-user/SGA_Apex_REST_API_WAR'
 mvn -e clean install
-mvn jetty:run
+mvn jetty:run >> /var/log/sga-apex-jetty.log &
