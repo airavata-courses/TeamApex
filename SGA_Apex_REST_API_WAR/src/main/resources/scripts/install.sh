d52eedf8ff8b2e8da9e81e19cb08085bb83a93eb
Gourav Shenoy <shenoy.200@gmail.com>
2016-04-19 03:36:37 -0400

    fix path issue in install.sh

--- SGA_Apex_REST_API_WAR/src/main/resources/scripts/install.sh
+++ SGA_Apex_REST_API_WAR/src/main/resources/scripts/install.sh
@@ -1,8 +1,8 @@
 echo 'Installing Science_Gateway_Apex to Maven'
-cd Science_Gateway_Apex
+cd /home/ec2-user/Science_Gateway_Apex
 mvn -e clean install
 
 echo 'Installing SGA_Apex_REST_API_WAR'
-cd ../SGA_Apex_REST_API_WAR
+cd /home/ec2-user/SGA_Apex_REST_API_WAR
 mvn -e clean install
 mvn jetty:run
