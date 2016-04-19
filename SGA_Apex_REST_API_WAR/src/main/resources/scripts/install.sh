a7489ce7e459d46d12f97cd7765ac756a6d64f29
Gourav Shenoy <shenoy.200@gmail.com>
2016-04-19 03:54:52 -0400

    convert sh files to use unix delimt

--- SGA_Apex_REST_API_WAR/src/main/resources/scripts/install.sh
+++ SGA_Apex_REST_API_WAR/src/main/resources/scripts/install.sh
@@ -1,7 +1,6 @@
 echo 'Installing Science_Gateway_Apex to Maven'
 cd '/home/ec2-user/Science_Gateway_Apex'
 mvn -e clean install
-
 echo 'Installing SGA_Apex_REST_API_WAR'
 cd '/home/ec2-user/SGA_Apex_REST_API_WAR'
 mvn -e clean install
