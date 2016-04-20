eea43990dd3e3660b7566fa722041a4c44b48b5d
Mangirish Wagle <vaglomangirish@gmail.com>
2016-04-20 11:10:59 -0400

    adding to script to replace notification email

--- Science_Gateway_Apex/src/main/resources/scripts/replace_props.sh
+++ Science_Gateway_Apex/src/main/resources/scripts/replace_props.sh
@@ -8,6 +8,7 @@
 username=$LOGIN_USER
 login_key_path=$LOGIN_PRIVATE_KEY_PATH
 login_key_passphrase=$LOGIN_KEY_PASSPHRASE
+mailto_email=$NOTIFICATION_EMAIL
 
 # Replacing in karst properties.
 sed -i 's~$user~'"$username"'~g' ../karst_config.properties
@@ -18,3 +19,9 @@ sed -i 's~$passphrase~'"$login_key_passphrase"'~g' ../karst_config.properties
 sed -i 's~$user~'"$username"'~g' ../bigred2_config.properties
 sed -i 's~$path~'"$login_key_path"'~g' ../bigred2_config.properties
 sed -i 's~$passphrase~'"$login_key_passphrase"'~g' ../bigred2_config.properties
+
+# Replacing in karst email properties.
+sed -i 's~$email~'"$mailto_email"'~g' karst/sendmail.properties
+
+# Replacing in bigred2 email properties.
+sed -i 's~$email~'"$mailto_email"'~g' karst/sendmail.properties
