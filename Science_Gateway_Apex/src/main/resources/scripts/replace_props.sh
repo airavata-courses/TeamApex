b498ec3b1750c691e186bb846b71a6f1a97c9e08
Mangirish Wagle <vaglomangirish@gmail.com>
2016-04-20 10:50:31 -0400

    correcting quotes

--- Science_Gateway_Apex/src/main/resources/scripts/replace_props.sh
+++ Science_Gateway_Apex/src/main/resources/scripts/replace_props.sh
@@ -10,11 +10,11 @@ login_key_path=$LOGIN_PRIVATE_KEY_PATH
 login_key_passphrase=$LOGIN_KEY_PASSPHRASE
 
 # Replacing in karst properties.
-sed -i "s/$user/${username}/g" ../karst_config.properties
-sed -i "s/$path/${login_key_path}/g" ../karst_config.properties
-sed -i "s/$passphrase/${login_key_passphrase}/g" ../karst_config.properties
+sed -i 's/$user/'"$username"'/g' ../karst_config.properties
+sed -i 's/$path/'"$login_key_path"'/g' ../karst_config.properties
+sed -i 's/$passphrase/'"$login_key_passphrase"'/g' ../karst_config.properties
 
 # Replacing in bigred2 properties.
-sed -i "s/$user/${username}/g" ../bigred2_config.properties
-sed -i "s/$path/${login_key_path}/g" ../bigred2_config.properties
-sed -i "s/$passphrase/${login_key_passphrase}/g" ../bigred2_config.properties
+sed -i 's/$user/'"$username"'/g' ../bigred2_config.properties
+sed -i 's/$path/'"$login_key_path"'/g' ../bigred2_config.properties
+sed -i 's/$passphrase/'"$login_key_passphrase"'/g' ../bigred2_config.properties
